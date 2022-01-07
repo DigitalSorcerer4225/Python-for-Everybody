70abcf13482319078421ce19184f118470b238df
echoes <92703060+DigitalSorcerer4225@users.noreply.github.com>
2022-01-07 10:55:18 +0800

    add chapter 4 exercises: gross-pay.py and grade.py

---
+++ chapter4-4.2-grade.py
@@ -0,0 +1,39 @@
+# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# My previous code:
+    # try:
+    #     s = float(input("Input % Grade:"))
+    # except:
+    #     s = "wrong"
+
+    # if s == "wrong":
+    #     print("Error, please input % Grade using numerical symbols only")
+    # elif s > 1:
+    #     print("Please input in these range of numbers: 0-1")
+    # elif s < 0:
+    #     print("Negative numbers are not possible input")
+    # elif s >= 0.9: print("A")
+    # elif s >= 0.8: print("B")
+    # elif s >= 0.7: print("C")
+    # elif s >= 0.6: print("D")
+    # elif s < 0.6: print("F")
